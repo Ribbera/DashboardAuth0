2fa50c9d90ab2e3e015da1e7ca09a1986c36b688
Ribbera <44053489+Ribbera@users.noreply.github.com>
2024-10-02 09:36:17 +0200

    DashboardRoute

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent }, // Default route
+  { path: '**', redirectTo: '' } // Wildcard route for a 404 page
+];
